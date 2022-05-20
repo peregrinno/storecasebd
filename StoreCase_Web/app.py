eb12e5e1e832a166f1ce621a6cc8e4fe5ccbb1d9
Luan Araújo <joseluan74@gmail.com>
2022-05-19 22:52:50 -0300

    Add files via upload

---
+++ StoreCase_Web/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+#criar pagina
+@app.route("/", methods=["GET", "POST"])
+def homepage():
+    variavel = "Cadastra na tabela produto"
+    
+    if request.method == "GET":
+        return render_template("homepage.html", variavel=variavel)
+    else:
+        categoria = request.form.get("categoria_produto")
+        tipo = request.form.get("tipo_produto")
+        preco = request.form.get("preco")
+
+        s_categoria = f'{categoria}'
+        s_tipo  = f'{tipo}'
+        s_preco = f'{preco}'
+        return render_template("homepage.html", categoria=s_categoria,tipo=s_tipo,preco=s_preco)
+
+#site online
+if __name__ == "__main__":
+    app.run(debug=True)
